2c49d11e90e87d8add83eca7c5c7d8358397dee1
Ismel.S <ssismel28@gmail.com>
2026-06-11 11:29:41 -0500

    Add CORS_ORIGINS configuration to config.py

--- config.py
+++ config.py
@@ -43,3 +43,6 @@ if not os.getenv("SECRET_KEY"):
 FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
 FLASK_PORT = int(os.getenv("FLASK_PORT", 5000))
 DASHBOARD_PASSWORD = os.getenv("DASHBOARD_PASSWORD")
+
+# ── CORS ALLOWED ────────────────────────────────────────────────────
+CORS_ORIGINS = os.getenv("CORS_ORIGINS", None)
